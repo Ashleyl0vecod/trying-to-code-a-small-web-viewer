7d1e87d7404623325079cfc7f4a9e9bdd513b1ea
Ashley Anderson <160888157+Ashleyl0vecod@users.noreply.github.com>
2024-02-22 18:13:22 +0200

    Create small try.js

---
+++ small try.js
@@ -0,0 +1,18 @@
+package strconv
+
+func commonPrefixLenIgnoreCase(s, prefix string) int {
+	n := len(prefix)
+	if n > len(s) {	sign := 1
+	nsign := 0
+	switch s[0] {
+	case '+', '-':}
+
+func (b *decimal) set(s string) (ok bool) {
+	i := 0
+	b.neg = false
+	b.trunc = false
+
+	// optional sign
+	if i >= len(s) {
+		return
+	}}
